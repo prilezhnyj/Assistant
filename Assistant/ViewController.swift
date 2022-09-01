0b30765d4ea6e86066f13e953a1fc0ae751f0e5d
Maksim Botalov <dev.botalov@gmail.com>
2022-09-01 21:01:39 +0500

    Initial Commit

---
+++ Assistant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assistant
+//
+//  Created by Максим Боталов on 01.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
