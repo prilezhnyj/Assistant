4df33d1a91dac88e41f63b16475a023c77e011e0
Maksim Botalov <dev.botalov@gmail.com>
2022-09-01 21:19:41 +0500

    Setup commit

---
+++ Assistant/Extension/ColorSetup.swift
@@ -0,0 +1,30 @@
+//
+//  ColorSetup.swift
+//  Assistant
+//
+//  Created by Максим Боталов on 01.09.2022.
+//
+
+import UIKit
+
+class ColorSetup {
+    static var background: UIColor {
+        return hexColor(value: "1C1C1E")
+    }
+    
+    static var gray: UIColor {
+        return hexColor(value: "3A3A3C")
+    }
+    
+    static var separator: UIColor {
+        return hexColor(value: "8E8E93")
+    }
+    
+    static var white: UIColor {
+        return hexColor(value: "FFFFFF")
+    }
+    
+    static var pink: UIColor {
+        return hexColor(value: "FF2D55")
+    }
+}
